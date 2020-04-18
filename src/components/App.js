dac260c293d005ce13a70fb2c21c18d3ee451700
Bigshrimp666 <pannijin@gmail.com>
2020-04-17 21:36:01 -0600

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Router, Route, Switch} from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamEdit from './streams/StreamEdit';
+import StreamDelete from './streams/StreamDelete';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
+import history from '../history';
+
+
+const App = () => {
+    return (
+        <div className="ui container">             
+            <Router history={history}>
+                <div>
+                    <Header />
+                    <Switch>
+                        <Route path="/" exact component={StreamList} />
+                        <Route path="/Streams/new" exact component={StreamCreate} />
+                        <Route path="/Streams/edit/:id" exact component={StreamEdit} />
+                        <Route path="/Streams/delete/:id" exact component={StreamDelete} />
+                        <Route path="/Streams/:id" exact component={StreamShow} />
+                    </Switch>
+                    
+                </div>               
+            </Router>
+        </div>
+    );
+}
+
+export default App;
